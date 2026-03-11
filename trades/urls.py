090a0efb86c6aac10741b1814ccb4788f6ce7e2f
fmnathpsfecyivpzzitlzlnp-blip <fmnathpsfecyivpzzitlzlnp@gmail.com>
2026-03-11 18:05:33 +0300

    Fix MT5 webhook routing and add Playbook matrix

--- trades/urls.py
+++ trades/urls.py
@@ -1,13 +1,18 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import TradeViewSet, PlaybookViewSet
+
+# 👇 ОБЯЗАТЕЛЬНО добавили mt5_webhook в импорт 👇
+from .views import TradeViewSet, PlaybookViewSet, mt5_webhook
 
 router = DefaultRouter()
 # Твои сделки
 router.register(r'trades', TradeViewSet, basename='trade')
-# 👇 НОВОЕ: API для Playbook
+# API для Playbook
 router.register(r'playbook', PlaybookViewSet, basename='playbook')
 
 urlpatterns = [
     path('', include(router.urls)),
+
+    # 👇 НОВОЕ: Учим Django принимать данные от советника по этому адресу 👇
+    path('mt5_webhook/', mt5_webhook, name='mt5_webhook'),
 ]
